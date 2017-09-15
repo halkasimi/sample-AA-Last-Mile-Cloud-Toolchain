bfc0d4ec45b9d9ee9295c5ed59ad5b9f732e43b3
halkasimi <halkasimi@hotmail.com>
2017-09-15 15:27:30 -0500

    Update hello_world.js

--- test/api/controllers/hello_world.js
+++ test/api/controllers/hello_world.js
@@ -36,7 +36,7 @@ describe('controllers', function() {
           .set('Accept', 'application/json')
           .expect('Content-Type', /json/)
           .expect(200)
-          .end(function(err, res) {
+          .end(function(err, res) { 
             should.not.exist(err); 
 
             res.body.should.eql('Hello, Scott!');
